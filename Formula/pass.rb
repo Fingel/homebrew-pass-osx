83a81839cef39265fb307d41f583e9527b67d02d
Austin Riba <austin@m51.io>
2021-01-20 16:45:41 -0800

    Update version

--- Formula/pass.rb
+++ Formula/pass.rb
@@ -1,9 +1,9 @@
 class Pass < Formula
   desc "Password manager"
   homepage "https://www.passwordstore.org/"
-  url "https://codeload.github.com/Fingel/password-store/zip/e660e55414369b322b78c8c6c2c4f2663"
-  sha256 "26a97117a3068a2fce4b71ee236c47d6766e21094734651b03ab9ed7bcd8f0c6"
-  version "ge660e55414"
+  url "https://codeload.github.com/Fingel/password-store/zip/5e9019f5bb407967d7777ad95a1a74f3517a1d43"
+  sha256 "a795c6bfbdf978f09e0c4b34317720893b8b76af58311e0f0b15305f14dc9e82"
+  version "5e9019f"
 
   depends_on "gnu-getopt"
   depends_on "gnupg"
